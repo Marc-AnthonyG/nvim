b93c249122b7bc55705d8710f243e12fd90639b2
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-04-01 11:44:42 -0400

    sdafasdf

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "query", "rust", "typescript", "python", "javascript" },
+  ensure_installed = { "c", "lua", "vim", "query", "rust", "typescript", "python", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

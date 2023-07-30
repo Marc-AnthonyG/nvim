51c52cf919ce8f4f8fd48a8b8a2dfb77be2418f5
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-07-29 22:12:14 -0400

    update nvim setup for go

--- lua/moi/packer.lua
+++ lua/moi/packer.lua
@@ -1,46 +1,48 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
   use { "catppuccin/nvim", as = "catppuccin" }
 
-	use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-	use ( 'nvim-treesitter/playground')
+  use 'nvim-treesitter/playground'
 
-	use ( 'theprimeagen/harpoon' )
+  use 'theprimeagen/harpoon'
 
-	use ('mbbill/undotree')
-	
-	use ( 'tpope/vim-fugitive' )
+  use 'mbbill/undotree'
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
+  use 'tpope/vim-fugitive'
 
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },     -- Required
+      {                                -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' },     -- Required
     }
-}
-    
-    use {'ThePrimeagen/vim-be-good'}
-end)
+  }
 
+  use 'ThePrimeagen/vim-be-good'
+
+  use 'mfussenegger/nvim-dap'
+  use {'leoluz/nvim-dap-go', require={'mfussenegger/nvim-dap'}}
+end)

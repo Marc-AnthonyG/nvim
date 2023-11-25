3f10acaf8677ac51b8cd06432a4fc885880a16fc
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-11-25 15:53:28 -0500

    finish transfering configuration

--- init.lua
+++ init.lua
@@ -46,16 +46,11 @@ require('lazy').setup({
 
   'tpope/vim-sleuth', -- Basicly copy indentation method of the current working directory
 
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-nvim-lsp', 'rafamadriz/friendly-snippets' },
-  },
 
-  { 'folke/which-key.nvim',                opts = {} },
+  { 'folke/which-key.nvim' },
 
+  -- Theme inspired by Atom
   {
-    -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
@@ -63,7 +58,8 @@ require('lazy').setup({
     end,
   },
 
-  {                              -- the line you can see at the bottom with the indication of the mode
+  -- the line you can see at the bottom with the indication of the mode
+  {
     'nvim-lualine/lualine.nvim', -- See `:help lualine.txt`
     opts = {
       options = {
@@ -81,102 +77,15 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim',               opts = {} },
 
-  { import = 'plugins' },
+  require 'plugins.treesitter',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocompletion',
 }, {})
 
 require('options')
 
 require('keymaps')
-
-
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
-require('mason').setup()
-require('mason-lspconfig').setup()
-
-local servers = {
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
-}
-
--- Setup neovim lua configuration
-require('neodev').setup()
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach, -- is calling on attach define in lsp config
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
-
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,5 +51,42 @@ return {
 				vim.lsp.buf.format()
 			end, { desc = 'Format current buffer with LSP' })
 		end
+		-- mason-lspconfig requires that these setup functions are called in this order
+		-- before setting up the servers.
+		require('mason').setup()
+		require('mason-lspconfig').setup()
+
+		local servers = {
+			lua_ls = {
+				Lua = {
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+				},
+			},
+		}
+
+		-- Setup neovim lua configuration
+		require('neodev').setup()
+		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+		-- Ensure the servers above are installed
+		local mason_lspconfig = require 'mason-lspconfig'
+
+		mason_lspconfig.setup {
+			ensure_installed = vim.tbl_keys(servers),
+		}
+
+		mason_lspconfig.setup_handlers {
+			function(server_name)
+				require('lspconfig')[server_name].setup {
+					capabilities = capabilities,
+					on_attach = on_attach, -- is calling on attach define in lsp config
+					settings = servers[server_name],
+					filetypes = (servers[server_name] or {}).filetypes,
+				}
+			end,
+		}
 	end
 }

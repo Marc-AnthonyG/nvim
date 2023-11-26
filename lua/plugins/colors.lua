ce3b91531b1b0e884beefc352c723877f0d4b1c9
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-11-25 21:06:05 -0500

    set background transparent

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -48,5 +48,7 @@ return {
     config = function()
         require("catppuccin").setup(opts)
         vim.cmd.colorscheme "catppuccin"
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 }

1efbbda4827592dd5a771ccfecd8968d07e5c0f4
Marc-AnthonyG <marcanthony.girard.pro@gmail.com>
2024-04-29 11:37:23 -0400

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,10 +10,8 @@ return {
 	config = function()
 		vim.defer_fn(function()
 			require('nvim-treesitter.configs').setup {
-				ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'markdown', 'markdown_inline' },
-
+				ensure_installed = { 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'markdown' },
 				auto_install = true,
-
 				highlight = { enable = true },
 				indent = { enable = true },
 				incremental_selection = {
